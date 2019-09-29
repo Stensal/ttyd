1793808d97cb5583e1a653b7e32be3a79b71682e
Ning Wang <email@ningwang.org>
2019-09-28 20:54:35 -0700

    run sh and prog as 1000 uid

--- docker/run_prog.sh
+++ docker/run_prog.sh
@@ -1,14 +1,5 @@
 #!/ishell_bin/sh
-/ishell_bin/env \
-    PATH=./ \
-    COMPILER_DISPLAY_NAME=stensal \
-    COMPILER_CHECK_NAME=stensal \
-    KLARAM_REMOVE_SRCLOC_PREFIX=/home/jail \
-    DTS_CHECK_SWITCH=0x00000f13 \
-    DTS_MEMORY_UNINIT_CHECK=warning \
-    DTS_COLORING_MSG=1 \
-    DTS_STUDENT_MODE=1 \
-    ./prog.exe $@
+/ishell_bin/env ./prog.exe $@
 
 exit_code=$?
 if [ "$exit_code" = "0" ]; then
--- docker/sh_in_cattlegrid.sh
+++ docker/sh_in_cattlegrid.sh
@@ -8,12 +8,22 @@ AS=$((128*1024*1024))
 CPU=$((6*60*60))
 DATA=$((128*1024*1024))
 
+chown 1000:1000 -R /ishell/workspace/wandbox/$cattlecell/store
+
 /usr/bin/env \
+    PS1='$ ' \
     HOME=/home/jail \
     PATH=.:/ishell_bin \
+    COMPILER_DISPLAY_NAME=stensal \
+    COMPILER_CHECK_NAME=stensal \
+    KLARAM_REMOVE_SRCLOC_PREFIX=/home/jail \
+    DTS_CHECK_SWITCH=0x00000f13 \
+    DTS_MEMORY_UNINIT_CHECK=warning \
+    DTS_COLORING_MSG=1 \
+    DTS_STUDENT_MODE=1 \
     /bin/prlimit \
     --core=0 --as=${AS} --cpu=${CPU} --data=${DATA} --fsize=${DATA} --nofile=16 --nproc=64 -- \
     /bin/cattlegrid --rootdir=./jail \
     --mount=/ishell_bin,/lib \
     --rwmount=/tmp=/tmp,/home/jail=/ishell/workspace/wandbox/$cattlecell/store \
-    --chdir=/home/jail /ishell_bin/sh +m
+    --chdir=/home/jail --uid=1000 /ishell_bin/sh +m
