b301c99358d4e9637d7883ebad8c7b50d648d26f
Ning Wang <email@ningwang.org>
2019-10-06 12:45:47 -0700

    the previous commit is to remove false security alerts, this one will register docker image

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -f Dockerfile-static . -t ttyd-static-docker-builder 
+docker build -f Dockerfile-static . -t dockerhub.stensalinc.com/x86_64/ttyd-static-docker-builder
+docker push dockerhub.stensalinc.com/x86_64/ttyd-static-docker-builder:latest
--- build-static-ttyd.sh
+++ build-static-ttyd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/build ttyd-static-docker-builder /bin/bash \
+docker run --rm -it -v $(pwd):/build dockerhub.stensalinc.com/x86_64/ttyd-static-docker-builder:latest /bin/bash \
     -c "cd /build/cmake-build-debug && rm -rf * && cmake -DCMAKE_BUILD_TYPE=Release -DSTATIC_TTYD=ON .. && make"
