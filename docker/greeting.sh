b6a754573c302948655ee9379e38472e2ce52f26
Ning Wang <email@ningwang.org>
2019-10-11 20:24:36 -0700

    no need to do something we dont know yet

--- docker/greeting.sh
+++ docker/greeting.sh
@@ -6,6 +6,7 @@ printf " The following shell commands are available.\n"
 printf " \t\e[1;32mls\e[0m -- list all files\n"
 printf " \t\e[1;32mclear\e[0m -- clear the console\n"
 printf " \t\e[1;32mexit\e[0m -- exit this console\n"
+printf " \t\e[1;32mCtrl-D\e[0m  -- terminate a running process or this console session\n"
 echo ""
 printf " Please run one of the following executables to test your code:\n"
 for i in *; do
@@ -14,7 +15,9 @@ for i in *; do
     fi
 done
 printf "\n\n"
+if [ "0" != "0" ]; then
 printf " We don't want to set up a short timeout for this \n"
 printf " session, please use the console responsibly. \n"
 printf " Close your browser or exit this console whenever \n"
 printf " you are done.\n\n"
+fi
