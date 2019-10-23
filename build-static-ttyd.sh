557857455cb28f41c4a3b3273bf676a2008829c2
Ning Wang <email@ningwang.org>
2019-10-23 08:46:28 -0700

    add dumb-init to wait for all orphan processes

--- build-static-ttyd.sh
+++ build-static-ttyd.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "build ttyd .."
 docker run --rm -it -v $(pwd):/build dockerhub.stensalinc.com/x86_64/ttyd-static-docker-builder:latest /bin/bash \
     -c "cd /build/static-build && rm -rf * && cmake -DCMAKE_BUILD_TYPE=Release -DSTATIC_TTYD=ON .. && make"
+
+
+echo "build dumb-init .."
+docker run --rm -it -v $(pwd):/build dockerhub.stensalinc.com/x86_64/ttyd-static-docker-builder:latest /bin/bash \
+    -c "cd /build/dumb-init && gcc -static dumb-init.c -o dumb-init"
