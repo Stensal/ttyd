3ee4eb86bdbad74ede5f5684a4ab22c99f0d5380
Ning Wang <email@ningwang.org>
2020-04-22 13:31:43 -0700

    add rm to allow users delete accidently upload binaries

--- docker/greeting.sh
+++ docker/greeting.sh
@@ -4,6 +4,7 @@ umask 022
 
 printf " The following shell commands are available.\n"
 printf " \t\e[1;32mls\e[0m -- list all files\n"
+printf " \t\e[1;32mrm\e[0m -- remove files or directories\n"
 printf " \t\e[1;32mclear\e[0m -- clear the console\n"
 printf " \t\e[1;32mexit\e[0m -- exit this console\n"
 printf " \t\e[1;32mCtrl-D\e[0m  -- terminate a running process or this console session\n"
