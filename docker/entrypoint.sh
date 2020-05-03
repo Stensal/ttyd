1627860389f4aab3e0ec2d494933496f86bdf87b
Ning Wang <email@ningwang.org>
2020-05-02 22:05:43 -0700

    add cattlegrid for cs50

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-GDBTEST=/ishell/workspace/sbox/t-gdbtest
-
-if [ -d ${GDBTEST} ]; then
-    chmod 770 ${GDBTEST}
-    rm -rf ${GDBTEST}
-fi
-cp -a /t-gdbtest /ishell/workspace/sbox
-cd ${GDBTEST}/store
-./build.sh
-find ${GDBTEST} -type d -exec chmod 0770 {} \;
-find ${GDBTEST} -type d -exec chown -R tester:0 {} \;
 # redirect stdout/stderr to files
 exec >>  /var/log/ishell-stdout.log
 exec 2>> /var/log/ishell-stderr.log
---
+++ docker/sh_in_cattlegrid_cs50.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -x
+mypath=$(dirname $(readlink -f $0))
+
+cattlecell=$1
+shift
+
+MEM=$((1024*1024*1024))
+AS=$MEM
+CPU=$((1*60*60))
+DATA=$MEM
+XUID=2000
+XGID=20000
+NFILES=256
+
+CELL=/ishell/workspace/sbox/$cattlecell
+STORE=${CELL}/store
+JAIL=${CELL}/jail
+
+if [ -d ${STORE} ]; then
+    cd ${CELL};
+    pwd;
+    cp /xshell/bin/greeting.sh   ${STORE}/.profile
+    # make all folders accessible
+    #find ${CELL} -type d -exec chmod 0777 {} \;
+
+    /usr/bin/env \
+	PS1='$ ' \
+	HOME=/home/user \
+	PATH=.:/bin:/xshell/bin:/usr/bin \
+	COMPILER_DISPLAY_NAME=stensal \
+	COMPILER_CHECK_NAME=stensal \
+	KLARAM_REMOVE_SRCLOC_PREFIX=/home/user \
+	DTS_CHECK_SWITCH=0x00000f13 \
+	DTS_MEMORY_UNINIT_CHECK=warning \
+	DTS_COLORING_MSG=1 \
+	DTS_GRACE_ABORT=0  \
+	DTS_STUDENT_MODE=1 \
+	DTS_REPORT_UNRELEASED_MEMORY=1 \
+	/bin/nice \
+	/bin/prlimit \
+	--core=0 --as=${AS} --cpu=${CPU} --data=${DATA} --fsize=${DATA} --nofile=${NFILES} --nproc=64 -- \
+	/bin/cattlegrid --rootdir=./ebox \
+	--mount=/bin,/sbin,/usr/bin,/xshell,/lib,/usr/lib,/sjacket/lib,/sjacket/usr/lib,/sjacket/etc,/usr/share/terminfo,/dev \
+	--rwmount=/etc/=/etc,/tmp=/tmp,/home/user=./store \
+    --uid=${XUID} \
+	--chdir=/home/user \
+	/xshell/bin/bash 
+else
+    #--devices=/dev/null,/dev/zero,/dev/full,/dev/random,/dev/urandom \
+    # possible attack
+    exit 1;
+fi
