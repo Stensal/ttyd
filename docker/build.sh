a027253eafb1042cce1192f7b4596f10a48f8957
mlite <mlite@users.noreply.github.com>
2020-12-13 19:39:41 -0800

    enable the ishell-cs50

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,6 @@ docker build . -f Dockerfile -t dockerhub.stensalinc.com/i386/ishell:latest
 docker push dockerhub.stensalinc.com/i386/ishell:latest
 docker image prune -f
 
-#docker build . -f Dockerfile_cs50 -t dockerhub.stensalinc.com/i386/ishell-cs50:latest
-#docker push dockerhub.stensalinc.com/i386/ishell-cs50:latest
-#docker image prune -f
+docker build . -f Dockerfile_cs50 -t dockerhub.stensalinc.com/i386/ishell-cs50:latest
+docker push dockerhub.stensalinc.com/i386/ishell-cs50:latest
+docker image prune -f
